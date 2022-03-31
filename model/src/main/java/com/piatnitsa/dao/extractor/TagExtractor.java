5a6de9de35a3f1b4f3e063a994f2b4dc9779540e
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-03-31 12:24:32 +0300

    Changed TagMapper to TagExtractor

---
+++ model/src/main/java/com/piatnitsa/dao/extractor/TagExtractor.java
@@ -0,0 +1,26 @@
+package com.piatnitsa.dao.extractor;
+
+import com.piatnitsa.entity.Tag;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TagExtractor implements ResultSetExtractor<List<Tag>> {
+
+    @Override
+    public List<Tag> extractData(ResultSet rs) throws SQLException, DataAccessException {
+        List<Tag> tags = new ArrayList<>();
+        while (rs.next()) {
+            Tag tag = new Tag();
+            tag.setId(rs.getLong("id"));
+            tag.setName(rs.getString("tag_name"));
+            tags.add(tag);
+        }
+        return tags;
+    }
+}
