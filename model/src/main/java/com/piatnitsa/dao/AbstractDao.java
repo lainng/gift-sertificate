e4dd1c57b05c603297359491d4afb2ba7bb4ca81
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-01 12:23:50 +0300

    Updated sql query handling

--- model/src/main/java/com/piatnitsa/dao/AbstractDao.java
+++ model/src/main/java/com/piatnitsa/dao/AbstractDao.java
@@ -1,11 +1,34 @@
 package com.piatnitsa.dao;
 
+import com.piatnitsa.exception.DaoException;
+import com.piatnitsa.exception.DaoExceptionMessageCodes;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
-public class AbstractDao<T> {
+import java.util.List;
+
+public abstract class AbstractDao<T> {
     protected final JdbcTemplate jdbcTemplate;
+    protected final ResultSetExtractor<List<T>> resultSetExtractor;
 
-    public AbstractDao(JdbcTemplate jdbcTemplate) {
+    public AbstractDao(JdbcTemplate jdbcTemplate, ResultSetExtractor<List<T>> resultSetExtractor) {
         this.jdbcTemplate = jdbcTemplate;
+        this.resultSetExtractor = resultSetExtractor;
+    }
+
+    public T executeQueryAsSimpleEntity(String query, Object... params) throws DaoException {
+        List<T> items = executeQuery(query, params);
+        if (items == null || items.size() == 0) {
+            throw new DaoException(DaoExceptionMessageCodes.NO_ENTITY_WITH_ID);
+        }
+        return items.get(0);
+    }
+
+    public List<T> executeQuery(String query, Object... params) {
+        return jdbcTemplate.query(query, resultSetExtractor, params);
+    }
+
+    public void executeUpdateQuery(String query, Object... params) {
+        jdbcTemplate.update(query, params);
     }
 }
--- model/src/main/java/com/piatnitsa/dao/impl/TagDaoImpl.java
+++ model/src/main/java/com/piatnitsa/dao/impl/TagDaoImpl.java
@@ -1,10 +1,10 @@
 package com.piatnitsa.dao.impl;
 
 import com.piatnitsa.dao.AbstractDao;
+import com.piatnitsa.dao.TagDao;
 import com.piatnitsa.dao.extractor.TagExtractor;
 import com.piatnitsa.entity.Tag;
 import com.piatnitsa.exception.DaoException;
-import com.piatnitsa.exception.DaoExceptionMessageCodes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -12,57 +12,41 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class TagDaoImpl extends AbstractDao<Tag> implements com.piatnitsa.dao.TagDao {
+public class TagDaoImpl extends AbstractDao<Tag> implements TagDao {
+    private static final String QUERY_SELECT_BY_ID = "select * from tag where id = ?;";
+    private static final String QUERY_SELECT_BY_NAME = "select * from tag where tag_name = ?;";
+    private static final String QUERY_SELECT_ALL_TAGS = "select * from tag;";
+    private static final String QUERY_INSERT_TAG = "insert into tag(tag_name) values (?);";
+    private static final String QUERY_DELETE_BY_ID = "delete from tag where id = ?;";
 
     @Autowired
-    public TagDaoImpl(JdbcTemplate jdbcTemplate) {
-        super(jdbcTemplate);
+    public TagDaoImpl(JdbcTemplate jdbcTemplate, TagExtractor resultSetExtractor) {
+        super(jdbcTemplate, resultSetExtractor);
     }
 
     @Override
     public Tag getById(long id) throws DaoException {
-        return jdbcTemplate.query(
-                        "select * from tag where id = ?;",
-                        new TagExtractor(),
-                        id
-                ).stream()
-                .findFirst()
-                .orElseThrow(() -> new DaoException(DaoExceptionMessageCodes.NO_ENTITY_WITH_ID));
+        return executeQueryAsSimpleEntity(QUERY_SELECT_BY_ID, id);
     }
 
     @Override
     public Tag getByName(String name) throws DaoException {
-        return jdbcTemplate.query(
-                "select * from tag where tag_name = ?;",
-                new TagExtractor(),
-                name
-        ).stream()
-                .findFirst()
-                .orElseThrow(() -> new DaoException(DaoExceptionMessageCodes.NO_ENTITY_WITH_ID));
+        return executeQueryAsSimpleEntity(QUERY_SELECT_BY_NAME, name);
     }
 
 
     @Override
     public List<Tag> getAll() {
-        return jdbcTemplate.query(
-                "select * from tag;",
-                new TagExtractor()
-        );
+        return executeQuery(QUERY_SELECT_ALL_TAGS);
     }
 
     @Override
     public void insert(Tag item) {
-        jdbcTemplate.update(
-                "insert into tag(tag_name) values (?);",
-                item.getName()
-        );
+        executeUpdateQuery(QUERY_INSERT_TAG, item.getName());
     }
 
     @Override
     public void removeById(long id) {
-        jdbcTemplate.update(
-                "delete from tag where id = ?;",
-                id
-        );
+        executeUpdateQuery(QUERY_DELETE_BY_ID, id);
     }
 }
