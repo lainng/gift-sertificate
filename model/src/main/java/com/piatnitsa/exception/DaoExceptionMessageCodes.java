25836e29b723b62a903ae31e931be0aba0510420
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-04 12:32:35 +0300

    Updated exception codes

--- model/src/main/java/com/piatnitsa/exception/DaoExceptionMessageCodes.java
+++ model/src/main/java/com/piatnitsa/exception/DaoExceptionMessageCodes.java
@@ -3,5 +3,4 @@ package com.piatnitsa.exception;
 public class DaoExceptionMessageCodes {
     public static final String NO_ENTITY_WITH_ID = "404001";
     public static final String NO_ENTITY_WITH_NAME = "404002";
-    public static final String SAVING_ERROR = "404004";
 }
