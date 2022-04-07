a65a301a42b859079718907a25ea81246f60563b
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-07 19:54:44 +0300

    Added AbstractService

---
+++ service/src/main/java/com/piatnitsa/service/AbstractService.java
@@ -0,0 +1,28 @@
+package com.piatnitsa.service;
+
+import com.piatnitsa.dao.CRDDao;
+import com.piatnitsa.exception.DaoException;
+
+import java.util.List;
+
+public abstract class AbstractService<T> implements CRDService<T> {
+    private final CRDDao<T> dao;
+
+    protected AbstractService(CRDDao<T> dao) {
+        this.dao = dao;
+    }
+    @Override
+    public T getById(long id) throws DaoException {
+        return dao.getById(id);
+    }
+
+    @Override
+    public List<T> getAll() {
+        return dao.getAll();
+    }
+
+    @Override
+    public void removeById(long id) throws DaoException {
+        dao.removeById(id);
+    }
+}
