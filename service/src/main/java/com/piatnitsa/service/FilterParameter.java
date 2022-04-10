3b837e303a517e0c7c795c7a41fc3d3884ddb55e
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-10 14:05:18 +0300

    Added FilterParameter

---
+++ service/src/main/java/com/piatnitsa/service/FilterParameter.java
@@ -0,0 +1,8 @@
+package com.piatnitsa.service;
+
+public final class FilterParameter {
+    public static final String NAME = "name";
+    public static final String TAG_NAME = "tag_name";
+    public static final String DESCRIPTION = "description";
+    public static final String SORT_BY_TAG_NAME = "tag_name_sort";
+}
