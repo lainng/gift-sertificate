26d99bd37a560f08274fec1c13aa83073a9f5aee
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-10 14:06:51 +0300

    Updated exception codes

--- model/src/main/java/com/piatnitsa/exception/DaoExceptionMessageCodes.java
+++ model/src/main/java/com/piatnitsa/exception/DaoExceptionMessageCodes.java
@@ -1,7 +1,7 @@
 package com.piatnitsa.exception;
 
-public class DaoExceptionMessageCodes {
-    public static final String INCORRECT_FILTER_PARAMETER = "40001";
+public final class DaoExceptionMessageCodes {
     public static final String NO_ENTITY_WITH_ID = "40401";
     public static final String NO_ENTITY_WITH_NAME = "40402";
+    public static final String NO_ENTITY_WITH_PARAMETERS = "40403";
 }
