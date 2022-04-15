d5bb1ee2de4911bbb24187ed44c5ddbd7a96feec
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-15 12:54:45 +0300

    Added new error code

--- model/src/main/java/com/piatnitsa/exception/DaoExceptionMessageCodes.java
+++ model/src/main/java/com/piatnitsa/exception/DaoExceptionMessageCodes.java
@@ -4,4 +4,5 @@ public final class DaoExceptionMessageCodes {
     public static final String NO_ENTITY_WITH_ID = "40401";
     public static final String NO_ENTITY_WITH_NAME = "40402";
     public static final String NO_ENTITIES_WITH_PARAMETERS = "40403";
+    public static final String SAVING_ERROR = "50001";
 }
