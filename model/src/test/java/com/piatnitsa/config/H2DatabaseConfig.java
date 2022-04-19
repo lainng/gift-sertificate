afcd22401e987b45ef99f0f1fd6e098470bea9d5
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-19 12:18:26 +0300

    Added H2 configuration

---
+++ model/src/test/java/com/piatnitsa/config/H2DatabaseConfig.java
@@ -0,0 +1,53 @@
+package com.piatnitsa.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.h2.jdbcx.JdbcDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.piatnitsa")
+@PropertySource("classpath:h2.properties")
+@Profile("test")
+public class H2DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource(HikariConfig config) {
+        HikariDataSource dataSource = new HikariDataSource(config);
+
+        Resource initData = new ClassPathResource("creatingTables.sql");
+        Resource fillData = new ClassPathResource("fillingTables.sql");
+        DatabasePopulator databasePopulator = new ResourceDatabasePopulator(initData, fillData);
+        DatabasePopulatorUtils.execute(databasePopulator, dataSource);
+        return dataSource;
+    }
+
+    @Bean
+    public HikariConfig hikariConfig(@Value("${h2.url}") String url,
+                                     @Value("${h2.user}") String username,
+                                     @Value("${h2.password}") String password,
+                                     @Value("${h2.driver}") String driverName,
+                                     @Value("${h2.connectionNumber}") int connectionNumber) {
+        HikariConfig config = new HikariConfig();
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setJdbcUrl(url);
+        config.setDriverClassName(driverName);
+        config.setMaximumPoolSize(connectionNumber);
+        return config;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
