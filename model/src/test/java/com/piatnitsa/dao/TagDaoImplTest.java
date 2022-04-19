9601aa0504c4a8ec828c7ba46147dfa3f28b38f3
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-19 19:15:35 +0300

    Added TagDaoImplTest.java

---
+++ model/src/test/java/com/piatnitsa/dao/TagDaoImplTest.java
@@ -0,0 +1,84 @@
+package com.piatnitsa.dao;
+
+import com.piatnitsa.config.H2DatabaseConfig;
+import com.piatnitsa.entity.Tag;
+import com.piatnitsa.exception.DaoException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@ContextConfiguration(classes = H2DatabaseConfig.class)
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+public class TagDaoImplTest {
+    private static final Tag TAG_1 = new Tag(1, "tagName1");
+    private static final Tag TAG_2 = new Tag(2, "tagName3");
+    private static final Tag TAG_3 = new Tag(3, "tagName2");
+    private static final Tag TAG_4 = new Tag(4, "tagName5");
+    private static final Tag TAG_5 = new Tag(5, "tagName4");
+    private static final String NOT_EXISTED_NAME = "not existed name";
+    private static final String PART_OF_TAG_NAME = "tagna";
+    private static final long NOT_EXISTED_ID = 6;
+    private static final String INCORRECT_PARAMETER_VALUE = "incorrect parameter value";
+    private static final String ASCENDING = "ASC";
+
+    @Autowired
+    private TagDao tagDao;
+
+    @Test
+    void getByIdTest_CorrectId() throws DaoException {
+        Tag actual = tagDao.getById(TAG_1.getId());
+        Assertions.assertEquals(TAG_1, actual);
+    }
+
+    @Test
+    void getByIdTest_NotExistedId() {
+        Assertions.assertThrows(DaoException.class, () -> tagDao.getById(NOT_EXISTED_ID));
+    }
+
+    @Test
+    void getAllTest() {
+        List<Tag> expectedTags = Arrays.asList(TAG_1, TAG_2, TAG_3, TAG_4, TAG_5);
+        List<Tag> actualTags = tagDao.getAll();
+        Assertions.assertEquals(expectedTags, actualTags);
+    }
+
+    @Test
+    void getByNameTest() throws DaoException {
+        Tag actual = tagDao.getByName(TAG_2.getName());
+        Assertions.assertEquals(TAG_2, actual);
+    }
+
+    @Test
+    void getByNameTest_NotExistedName() {
+        Assertions.assertThrows(DaoException.class, () -> tagDao.getByName(NOT_EXISTED_NAME));
+    }
+
+    @Test
+    void getWithFiltersTest_CorrectParams() throws DaoException {
+        Map<String, String> filterParams = new LinkedHashMap<>();
+        filterParams.put("tag_name", PART_OF_TAG_NAME);
+        filterParams.put("tag_name_sort", ASCENDING);
+        List<Tag> actual = tagDao.getWithFilter(filterParams);
+        List<Tag> expected = Arrays.asList(TAG_1, TAG_3, TAG_2, TAG_5, TAG_4);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void getWithFiltersTest_IncorrectParam() {
+        Map<String, String> filterParams = new LinkedHashMap<>();
+        filterParams.put("tag_name", PART_OF_TAG_NAME);
+        filterParams.put("incorrect_param", INCORRECT_PARAMETER_VALUE);
+        Assertions.assertThrows(DaoException.class, () -> tagDao.getWithFilter(filterParams));
+    }
+
+}
