f72ca092ecb8c0f613421ac022576c98316fe514
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-19 21:25:30 +0300

    Added column name classes

---
+++ model/src/main/java/com/piatnitsa/entity/GiftCertificateColumnName.java
@@ -0,0 +1,12 @@
+package com.piatnitsa.entity;
+
+public class GiftCertificateColumnName {
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String DESCRIPTION = "description";
+    public static final String PRICE = "price";
+    public static final String DURATION = "duration";
+    public static final String CREATE_DATE = "create_date";
+    public static final String LAST_UPDATE_DATE = "last_update_date";
+    public static final String TAGS = "tags";
+}
---
+++ model/src/main/java/com/piatnitsa/entity/TagColumnName.java
@@ -0,0 +1,6 @@
+package com.piatnitsa.entity;
+
+public class TagColumnName {
+    public static final String ID = "id";
+    public static final String TAG_NAME = "tag_name";
+}
