6f8d89d2e495ddc5f7e9d9dbe42f5aaa925440f4
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-20 21:13:32 +0300

    Added scripts for empty entity list test

---
+++ model/src/test/resources/fillingTagTable.sql
@@ -0,0 +1,15 @@
+/* --------------filling in the table "tags"---------------------*/
+INSERT INTO tag (id, tag_name)
+VALUES ('1', 'tagName1');
+
+INSERT INTO tag (id, tag_name)
+VALUES ('2', 'tagName3');
+
+INSERT INTO tag (id, tag_name)
+VALUES ('3', 'tagName2');
+
+INSERT INTO tag (id, tag_name)
+VALUES ('4', 'tagName5');
+
+INSERT INTO tag (id, tag_name)
+VALUES ('5', 'tagName4');
