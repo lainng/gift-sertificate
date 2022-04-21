83b94e3c32cb81b727a539fe119cdb370f3f7681
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-21 22:24:52 +0300

    Added new exception codes

--- service/src/main/java/com/piatnitsa/exception/IncorrectParameterMessageCodes.java
+++ service/src/main/java/com/piatnitsa/exception/IncorrectParameterMessageCodes.java
@@ -2,4 +2,12 @@ package com.piatnitsa.exception;
 
 public final class IncorrectParameterMessageCodes {
     public static final String BAD_TAG_FILTER_PARAMETER = "40001";
+    public static final String BAD_GIFT_CERTIFICATE_FILTER_PARAMETER = "40002";
+    public static final String BAD_ID = "40003";
+    public static final String BAD_TAG_NAME = "40004";
+    public static final String BAD_SORTING_TYPE = "40005";
+    public static final String BAD_GIFT_CERTIFICATE_NAME = "40006";
+    public static final String BAD_GIFT_CERTIFICATE_DESCRIPTION = "40007";
+    public static final String BAD_GIFT_CERTIFICATE_PRICE = "40008";
+    public static final String BAD_GIFT_CERTIFICATE_DURATION = "40009";
 }
