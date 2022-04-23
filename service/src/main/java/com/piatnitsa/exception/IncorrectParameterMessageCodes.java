ac77396d8afd6d047914d2eb54f0d9b7016c216e
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-23 15:07:00 +0300

    Added new error message code

--- service/src/main/java/com/piatnitsa/exception/IncorrectParameterMessageCodes.java
+++ service/src/main/java/com/piatnitsa/exception/IncorrectParameterMessageCodes.java
@@ -10,4 +10,5 @@ public final class IncorrectParameterMessageCodes {
     public static final String BAD_GIFT_CERTIFICATE_DESCRIPTION = "40007";
     public static final String BAD_GIFT_CERTIFICATE_PRICE = "40008";
     public static final String BAD_GIFT_CERTIFICATE_DURATION = "40009";
+    public static final String EMPTY_CERTIFICATE = "40010";
 }
