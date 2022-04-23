fd50f528b33087704ee20959f00cbe2d3747d997
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-23 16:00:07 +0300

    Code refactoring

--- web/src/main/java/com/piatnitsa/config/language/ExceptionMessageTranslator.java
+++ web/src/main/java/com/piatnitsa/config/language/ExceptionMessageTranslator.java
@@ -1,18 +1,18 @@
 package com.piatnitsa.config.language;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
 @Component
 public class ExceptionMessageTranslator {
-    private static ResourceBundleMessageSource messageSource;
+    private static MessageSource messageSource;
 
     @Autowired
-    public ExceptionMessageTranslator(ResourceBundleMessageSource messageSource) {
+    public ExceptionMessageTranslator(MessageSource messageSource) {
         ExceptionMessageTranslator.messageSource = messageSource;
     }
 
