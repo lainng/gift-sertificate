8a6c119cdaf86fa0be9aa8eb59c2bd6b7a7407f1
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-25 12:37:28 +0300

    Added new exception handlers

--- web/src/main/java/com/piatnitsa/exception/ExceptionsHandler.java
+++ web/src/main/java/com/piatnitsa/exception/ExceptionsHandler.java
@@ -1,10 +1,14 @@
 package com.piatnitsa.exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.piatnitsa.config.language.ExceptionMessageTranslator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class ExceptionsHandler {
@@ -13,7 +17,7 @@ public class ExceptionsHandler {
             DaoException.class,
             IncorrectParameterException.class
     })
-    public ResponseEntity<ErrorResponse> handleDaoExceptions(Exception ex) {
+    public ResponseEntity<ErrorResponse> handleExceptions(Exception ex) {
         ErrorResponse errorResponse = new ErrorResponse();
 
         String errorCode = ex.getMessage();
@@ -23,6 +27,36 @@ public class ExceptionsHandler {
         errorResponse.setErrorCode(errorCode + " " + httpStatus.getReasonPhrase());
         errorResponse.setErrorMessage(details);
         return new ResponseEntity<>(errorResponse, httpStatus);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public final ResponseEntity<ErrorResponse> methodNotAllowedExceptionException() {
+        ErrorResponse errorResponse = new ErrorResponse();
+        String details = ExceptionMessageTranslator.toLocale("exception.notSupported");
+        errorResponse.setErrorCode(HttpStatus.METHOD_NOT_ALLOWED.toString());
+        errorResponse.setErrorMessage(details);
+        return new ResponseEntity<>(errorResponse, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler({
+            MethodArgumentTypeMismatchException.class,
+            JsonProcessingException.class
+    })
+    public final ResponseEntity<ErrorResponse> handleBadRequestExceptions() {
+        ErrorResponse errorResponse = new ErrorResponse();
+        String details = ExceptionMessageTranslator.toLocale("exception.badRequest");
+        errorResponse.setErrorMessage(details);
+        errorResponse.setErrorCode(HttpStatus.BAD_REQUEST.toString());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public final ResponseEntity<ErrorResponse> handleBadRequestException() {
+        ErrorResponse errorResponse = new ErrorResponse();
+        String details = ExceptionMessageTranslator.toLocale("exception.noHandler");
+        errorResponse.setErrorMessage(details);
+        errorResponse.setErrorCode(HttpStatus.NOT_FOUND.toString());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     private HttpStatus parseHttpStatus(String exCode) {
