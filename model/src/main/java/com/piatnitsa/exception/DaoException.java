b2073b96a06dc1fc9376f7981eeb161cbf5013ce
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-04-25 21:15:35 +0300

    Added javadoc

--- model/src/main/java/com/piatnitsa/exception/DaoException.java
+++ model/src/main/java/com/piatnitsa/exception/DaoException.java
@@ -1,5 +1,13 @@
 package com.piatnitsa.exception;
 
+/**
+ * This class is an exception that is thrown when the requested resource is not found in the database
+ * or there was an error saving it.
+ *
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ * @see Exception
+ */
 public class DaoException extends Exception {
     public DaoException() {
         super();
--- model/src/main/java/com/piatnitsa/exception/DaoExceptionMessageCodes.java
+++ model/src/main/java/com/piatnitsa/exception/DaoExceptionMessageCodes.java
@@ -1,5 +1,12 @@
 package com.piatnitsa.exception;
 
+/**
+ * This class presents keys by which messages will be taken from properties files.
+ *
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ * @see DaoException
+ */
 public final class DaoExceptionMessageCodes {
     public static final String NO_ENTITY_WITH_ID = "40401";
     public static final String NO_ENTITY_WITH_NAME = "40402";
